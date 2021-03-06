8b3d02af35bd3cb14fa4bf2d26d924f46b30aec4
dharmawinata <dharma.winata92@gmail.com>
2021-03-07 01:56:56 +0700

    add conditional tugasPertemuan1 variable

---
+++ tugasPertemuan1.js
@@ -0,0 +1,138 @@
+// TASK - ALGORITMA & PSEUDOCODE
+
+// 1. Buat algoritma : How to make Indomie
+// ALGORITMA
+/**
+ * Ambil panci
+ * Isi dengan air
+ * Nyalakan kompor
+ * Letakkan panci yang telah diisi air diatas kompor
+ * Ambil indomie yang mau dimasak
+ * Buka bungkus indomie, lalu buka bumbunya dan tuangkan ke atas piring
+ * Masukkan indomie kedalam panci jika air sudah mendidih
+ * Tunggu indomie hingga matang lalu tiriskan airnya
+ * Campurkan indomie ke dalam piring yang berisi bumbu, aduk hingga merata
+ * Sajikan
+ */
+
+// 2. Buat algoritma : How to cook Sirloin Steak
+// ALGORITMA
+/**
+ * Ambil sirloin
+ * Cuci dengan air dingin di kedua sisinya, kemudian tepuk-tepuk hingga kering dengan tisu tebal
+ * Taburkan garam dan merica di kedua sisi
+ * Tambahkan bubuk bawang putih,cabai cayenne, bbubk cabai, atau bumbu-bumbu italia untuk variasi
+ * Diamkan daging didalam suhu ruangan selama satu jam sebelum dimasak
+ * Letakkan wajan diatas kompor dengan api sedang
+ * Masukkan satu atau dua sendok teh minyak goreng dan biarkan hingga panas berasap
+ * Masukkan daging yang sudah dibumbui tadi
+ * Bolak-balik daging di masing-masing sisinya setiap 30 detik hingga matang
+ *  -Untuk steak mentah (rare), masak dengan total waktu 1,5 menit tiap sisinya
+ *  -Untuk steak setengah mentah (medium rare), masak dengan total waktu 2 menit tiap sisinya
+ *  -Untuk steak setengah matang (medium well), masak dengan total waktu 2,5 menit tiap sisinya
+ *  -Untuk steak matang (well done), masak dengan total waktu 3 menit tiap sisinya
+ * Angkat steak dari wajan dan biarkan selama 3 menit
+ * Lalu sajikan
+ */
+
+// 3. Buat algoritma dan pseudocode : Luas persegi panjang
+// ALGORITMA
+/**
+ * Tentukan panjang
+ * Tentukan lebar
+ * Tentukan rumus
+ * Rumus : panjang * lebar
+ * Tampilkan luas
+ */
+
+// PSEUDOCODE
+/**
+ * STORE panjang WITH ANY NUMBER
+ * STORE lebar WITH ANY NUMBER
+ * SET luasPersegiPanjang WITH panjang TIMES lebar
+ * DISPLAY luasPersegiPanjang
+ */
+
+// 4. Buat algoritma dan pseudocode : Volume bola
+// ALGORITMA
+/**
+ * Tentukan jari-jari
+ * Tentukan phi
+ * Tentukan rumus
+ * Rumus : 4/3 * phi * jari-jari * jari-jari * jari-jari
+ * Tampilkan volumeBola
+ */
+
+// PSEUDOCODE
+/**
+ * STORE jari-jari WITH ANY NUMBER
+ * SET phi = 3.14
+ * SET volumeBola WITH 4/3 TIMES phi TIMES jari-jari TIMES jari-jari TIMES jari-jari
+ * DISPLAY volumeBola
+ */
+
+// 5. Buat algoritma : How to add, commit, push to Github
+// ALGORITMA
+/**
+ * 1. How to add to Github
+ *  1.1 buka terminal 
+ *  1.2 masukkan syntax berikut : git add .
+ *  1.3 lalu tekan ENTER
+ * 
+ * 2. How to commit to Github
+ *  2.1 buka terminal
+ *  2.2 masukkan syntax berikut : git commit -m "tulis keterangannya"
+ *  2.3 lalu tekan ENTER
+ * 
+ * 3. How to push to Github
+ *  3.1 buka terminal
+ *  3.2 masukkan syntax berikut : git push origin main
+ *  3.3 lalu tekan ENTER
+ */ 
+
+
+//  TASK - VARIABLES, OPERATIONS, JAVASCRIPT
+
+//  1. Buat algoritma, pseudocode, dan codingan : Keliling persegi
+
+// ALGORITMA
+/**
+ * Tentukan sisi
+ * Tentukan rumus
+ * Rumus : 4 * sisi
+ * Tampilkan kelilingPersegi
+ */
+
+// PSEUDOCODE
+/**
+ * STORE sisi WITH ANY NUMBER
+ * SET rumus WITH 4 TIMES sisi
+ * DISPLAY kelilingPersegi
+ */
+
+// CODING
+var sisi = 6;
+var kelilingPersegi = 4 * sisi;
+console.log(kelilingPersegi);
+ 
+//  2. Buat algoritma, pseudocode, dan codingan : Luas permukaan balok
+ 
+//  3.  Buat kodingan, untuk menjawab pertanyaan " Who are you ?" menggunakan variable.
+ 
+//      //Code here
+//      var name;
+//      var age;
+ 
+//      //Output
+//      //My name is Vincent, and i'm 28 years old.
+
+//  4. Buat kodingan, untuk membuat random number. Range number 1 - 10.
+ 
+//      //Code here
+//      var number;
+ 
+//      //Output 
+//      //Your number is 1
+//      //Your number is 5
+
+//  5. Buat kodingan, untuk membuat Nomor Induk mahasiswa. Rumus : <Nama_Mahasiswa><Nomor_Absen><Lulus/Tidak> Contoh : James15true dengan syarat nomor absen random, Range 1 - 50
--- variable.js
+++ variable.js
@@ -1,7 +1,37 @@
-var subject = "dharma";
-var tobe = "is";
-var verbing = "learning";
-var object = "JavaScript";
+// var subject = "dharma";
+// var tobe = "is";
+// var verbing = "learning";
+// var object = "JavaScript";
 
-var sentence = subject + tobe + verbing + object;
-console.log(sentence);
+// var sentence = subject + tobe + verbing + object;
+// console.log(sentence);
+
+
+// OPERATOR LOGIC
+// AudioNode, OR, NOT, == , ===
+// == operator ini untuk mebandingkan nilai
+// === operator ini untuk membandingkan nilai dan juga type data
+
+// var x = 10;
+// var y = 10;
+
+// var nilai = x == y;
+// console.log(nilai); 
+// ---> hasilnya TRUE
+
+// var x = "10";
+// var y = 10;
+
+// var nilai = x === y;
+// console.log(nilai); 
+// ---> hasilnya FALSE
+
+var x = "10";
+var y = 10;
+
+var a = x === y; // FALSE
+var b = x == y; // TRUE
+var c = a != b; // apakah false sama dengan true? TRUE
+
+console.log(c); 
+
